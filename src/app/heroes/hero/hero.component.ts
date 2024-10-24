358853e50b03f3a4fd2f4042f122267e18251e36
rolandoandre123 <rbardalespriale@gmail.com>
2024-10-24 01:17:04 -0500

    add : heroe components and get property method and encapsuling css on his own component

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class HeroComponent {
 
+  public name: string = 'ironman';
+  public age: number = 45;
+
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  public getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  public changeName(): void {
+    this.name = 'spiderman';
+  }
+
+  public changeAge(): void {
+    this.age = 50;
+  }
+
 }
